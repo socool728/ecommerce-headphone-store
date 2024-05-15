4ff1c340de58398b6980a9d6eb79bdfb048f125f
root <root@consultwithrabih.com>
2024-05-14 23:38:29 -0400

    little changed

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -10,9 +10,6 @@ export default async function handler(req, res) {
         mode: 'payment',
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
-        shipping_options: [
-          { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
-        ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
           const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
